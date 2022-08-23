8c6206de141f043bcb0ab3b28c9b166475a95c23
Billy McFall <22157057+Billy99@users.noreply.github.com>
2022-08-23 17:41:57 -0400

    Drop capabilities on spawned thread
    Spawned thread doesn't need the extra capabilities, so drop them.
    
    Signed-off-by: Billy McFall <22157057+Billy99@users.noreply.github.com>

--- bpfd/src/main.rs
+++ bpfd/src/main.rs
@@ -5,9 +5,11 @@ use std::{env, str::FromStr};
 
 use aya::include_bytes_aligned;
 use bpfd::server::{config_from_file, programs_from_directory, serve};
+use log::{debug, error};
 use nix::{
     libc::RLIM_INFINITY,
     sys::resource::{setrlimit, Resource},
+    unistd,
 };
 use systemd_journal_logger::{connected_to_journal, init_with_extra_fields};
 
@@ -15,29 +17,38 @@ const DEFAULT_BPFD_CONFIG_PATH: &str = "/etc/bpfd/bpfd.toml";
 const DEFAULT_BPFD_STATIC_PROGRAM_DIR: &str = "/etc/bpfd/programs.d";
 const BPFD_ENV_LOG_LEVEL: &str = "RUST_LOG";
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    if connected_to_journal() {
-        // If bpfd is running as a service, log to journald.
-        init_with_extra_fields(vec![("VERSION", env!("CARGO_PKG_VERSION"))]).unwrap();
-        manage_journal_log_level();
-        log::info!("Log using journald");
-    } else {
-        // Otherwise fall back to logging to standard error.
-        env_logger::init();
-        log::info!("Log using env_logger");
-    }
+fn main() -> anyhow::Result<()> {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .on_thread_start(|| {
+            drop_linux_capabilities();
+        })
+        .build()
+        .unwrap()
+        .block_on(async {
+            if connected_to_journal() {
+                // If bpfd is running as a service, log to journald.
+                init_with_extra_fields(vec![("VERSION", env!("CARGO_PKG_VERSION"))]).unwrap();
+                manage_journal_log_level();
+                log::info!("Log using journald");
+            } else {
+                // Otherwise fall back to logging to standard error.
+                env_logger::init();
+                log::info!("Log using env_logger");
+            }
 
-    let dispatcher_bytes =
-        include_bytes_aligned!("../../target/bpfel-unknown-none/release/xdp_dispatcher.bpf.o");
-    setrlimit(Resource::RLIMIT_MEMLOCK, RLIM_INFINITY, RLIM_INFINITY).unwrap();
+            let dispatcher_bytes = include_bytes_aligned!(
+                "../../target/bpfel-unknown-none/release/xdp_dispatcher.bpf.o"
+            );
+            setrlimit(Resource::RLIMIT_MEMLOCK, RLIM_INFINITY, RLIM_INFINITY).unwrap();
 
-    let config = config_from_file(DEFAULT_BPFD_CONFIG_PATH);
+            let config = config_from_file(DEFAULT_BPFD_CONFIG_PATH);
 
-    let static_programs = programs_from_directory(DEFAULT_BPFD_STATIC_PROGRAM_DIR)?;
+            let static_programs = programs_from_directory(DEFAULT_BPFD_STATIC_PROGRAM_DIR)?;
 
-    serve(config, dispatcher_bytes, static_programs).await?;
-    Ok(())
+            serve(config, dispatcher_bytes, static_programs).await?;
+            Ok(())
+        })
 }
 
 fn manage_journal_log_level() {
@@ -51,4 +62,25 @@ fn manage_journal_log_level() {
             Err(e) => log::error!("Invalid Log Level: {}", e),
         }
     }
+}
+
+fn drop_all_cap(cap: caps::CapSet) {
+    match caps::clear(None, cap) {
+        Ok(()) => debug!("CAPS:  {:?} Cleared", cap),
+        Err(e) => error!("CAPS:  Clear {:?} Error  {}", cap, e),
+    }
+}
+
+fn drop_linux_capabilities() {
+    debug!(
+        "CAPS: PID {:?} TID {:?} User {:?}",
+        unistd::getpid(),
+        unistd::gettid(),
+        unistd::getuid()
+    );
+    drop_all_cap(caps::CapSet::Ambient);
+    drop_all_cap(caps::CapSet::Bounding);
+    drop_all_cap(caps::CapSet::Effective);
+    drop_all_cap(caps::CapSet::Inheritable);
+    drop_all_cap(caps::CapSet::Permitted);
 }
