fa27bdd70431b5588982a8853f08e3e52e8156fe
Brent Salisbury <bsalisbu@redhat.com>
2022-09-14 02:26:25 -0400

    Integration tests and spawning bpfd process for IT
    - IT tests adds programs, verifies they were installed
      and then deletes them.
    - Added a utils module for routine tasks and broke out
      the bpfd process spawn from the main module.
    
    Signed-off-by: Brent Salisbury <bsalisbu@redhat.com>

--- tests/integration-test/src/tests/basic.rs
+++ tests/integration-test/src/tests/basic.rs
@@ -1,30 +1,59 @@
-use std::{thread::sleep, time::Duration};
+use std::{process::exit, thread::sleep, time::Duration};
 
-use assert_cmd::Command;
-use log::debug;
+use log::{debug, error};
 
 use super::{integration_test, IntegrationTest};
+use crate::tests::utils::*;
 
 #[integration_test]
 fn test_load_unload() -> anyhow::Result<()> {
-    let output = Command::cargo_bin("bpfctl")?
-        .args([
-            "load",
-            "--iface",
-            "eth0",
-            "--priority",
-            "50",
-            "--from-image",
-            "quay.io/astoycos/xdp_pass",
-        ])
-        .ok()?;
-    let uuid = String::from_utf8(output.stdout)?;
-    debug!("UUID is {:?}", uuid);
+    let _guard = start_bpfd()?;
+
+    let bpfd_iface = read_iface_env();
+    if !iface_exists(bpfd_iface) {
+        error!(
+            "Interface {} not found, specify a usable interface with the env BPFD_IFACE",
+            bpfd_iface
+        );
+        exit(0)
+    }
+
+    debug!("Installing xdp_pass programs");
+    let mut uuids = vec![];
+    // Install a few xdp programs
+    let uuid = add_xdp_pass(bpfd_iface, "75");
+    uuids.push(uuid.unwrap());
+    let uuid = add_xdp_pass(bpfd_iface, "50");
+    uuids.push(uuid.unwrap());
+    let uuid = add_xdp_pass(bpfd_iface, "100");
+    uuids.push(uuid.unwrap());
+    let uuid = add_xdp_pass(bpfd_iface, "25");
+    uuids.push(uuid.unwrap());
+    assert_eq!(uuids.len(), 4);
+
+    // Verify bpfctl list contains the uuids of each program
+    let bpfctl_list = bpfd_list(bpfd_iface);
+    let bpfctl_list = bpfctl_list.as_ref();
+    for id in uuids.iter() {
+        let prog_list = bpfctl_list.unwrap();
+        assert!(prog_list.contains(id));
+    }
+
+    // Delete the installed programs
+    debug!("Deleting bpfd programs");
     sleep(Duration::from_secs(2));
-    Command::cargo_bin("bpfctl")?
-        .args(["unload", "--iface", "eth0", uuid.trim()])
-        .assert()
-        .success();
-    debug!("Successfully deleted program: {}", uuid);
+    for id in uuids.iter() {
+        bpfd_del_program(bpfd_iface, id)
+    }
+
+    // Verify bpfctl list does not contain the uuids of the deleted programs
+    // and that there are no panics if bpfctl does not contain any programs.
+    let bpfctl_list = bpfd_list(bpfd_iface);
+    let bpfctl_list = bpfctl_list.as_ref();
+    for id in uuids.iter() {
+        let prog_list = bpfctl_list.unwrap();
+        assert!(!prog_list.contains(id));
+    }
+
     Ok(())
 }
