df0a68a74d1000e4c4dc4fe26060a28a0b8a77b3
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-05-03 11:31:39 +0200

    Initiate shutdown from the main task
    Have the signal handling task signal the main task that we should stop,
    this task in turn will handle signaling all other tasks and join them
    in order to propagate any errors.
    
    Signed-off-by: Mauro Ezequiel Moltrasio <mmoltras@redhat.com>

--- bpfd/src/lib.rs
+++ bpfd/src/lib.rs
@@ -28,7 +28,7 @@ use tokio::{
     net::UnixListener,
     select,
     signal::unix::{signal, SignalKind},
-    sync::mpsc,
+    sync::{mpsc, watch},
 };
 use tokio_stream::wrappers::UnixListenerStream;
 use tonic::transport::{Server, ServerTlsConfig};
@@ -106,6 +106,7 @@ pub async fn serve(config: Config, static_program_path: &str) -> anyhow::Result<
         }
     }));
 
+    let (shutdown_tx, mut shutdown_rx) = watch::channel(());
     task_handles.push(tokio::spawn(async move {
         let mut sigint = signal(SignalKind::interrupt()).unwrap();
         let mut sigterm = signal(SignalKind::terminate()).unwrap();
@@ -114,9 +115,7 @@ pub async fn serve(config: Config, static_program_path: &str) -> anyhow::Result<
             _ = sigterm.recv() => {debug!("Received SIGTERM")},
         }
 
-        for handle in shutdown_senders {
-            handle.send(()).unwrap_or_default();
-        }
+        shutdown_tx.send(()).unwrap();
     }));
 
     let mut bpf_manager = BpfManager::new(&config);
@@ -132,156 +131,168 @@ pub async fn serve(config: Config, static_program_path: &str) -> anyhow::Result<
         }
     };
 
-    // Start receiving messages
-    while let Some(cmd) = rx.recv().await {
-        match cmd {
-            Command::LoadXDP {
-                location,
-                section_name,
-                id,
-                global_data,
-                iface,
-                priority,
-                proceed_on,
-                username,
-                responder,
-            } => {
-                let res = if let Ok(if_index) = get_ifindex(&iface) {
-                    let prog_data =
-                        ProgramData::new(location, section_name.clone(), global_data, username)
-                            .await?;
+    loop {
+        select! {
+            biased;
 
-                    let prog_result = Ok(Program::Xdp(XdpProgram {
-                        data: prog_data.clone(),
-                        info: XdpProgramInfo {
-                            if_index,
-                            current_position: None,
-                            metadata: command::Metadata {
-                                priority,
-                                // This could have been overridden by image tags
-                                name: prog_data.section_name,
-                                attached: false,
-                            },
-                            proceed_on,
-                            if_name: iface,
-                        },
-                    }));
+            _ = shutdown_rx.changed() => break,
 
-                    match prog_result {
-                        Ok(prog) => bpf_manager.add_program(prog, id).await,
-                        Err(e) => Err(e),
+            // Start receiving messages
+            Some(cmd) = rx.recv() => {
+                match cmd {
+                    Command::LoadXDP {
+                        location,
+                        section_name,
+                        id,
+                        global_data,
+                        iface,
+                        priority,
+                        proceed_on,
+                        username,
+                        responder,
+                    } => {
+                        let res = if let Ok(if_index) = get_ifindex(&iface) {
+                            let prog_data =
+                                ProgramData::new(location, section_name.clone(), global_data, username)
+                                    .await?;
+
+                            let prog_result = Ok(Program::Xdp(XdpProgram {
+                                data: prog_data.clone(),
+                                info: XdpProgramInfo {
+                                    if_index,
+                                    current_position: None,
+                                    metadata: command::Metadata {
+                                        priority,
+                                        // This could have been overridden by image tags
+                                        name: prog_data.section_name,
+                                        attached: false,
+                                    },
+                                    proceed_on,
+                                    if_name: iface,
+                                },
+                            }));
+
+                            match prog_result {
+                                Ok(prog) => bpf_manager.add_program(prog, id).await,
+                                Err(e) => Err(e),
+                            }
+                        } else {
+                            Err(BpfdError::InvalidInterface)
+                        };
+
+                        // If program was successfully loaded, allow map access by bpfd group members.
+                        if let Ok(uuid) = &res {
+                            let maps_dir = format!("{RTDIR_FS_MAPS}/{uuid}");
+                            set_dir_permissions(&maps_dir, MAPS_MODE).await;
+                        }
+
+                        // Ignore errors as they'll be propagated to caller in the RPC status
+                        let _ = responder.send(res);
                     }
-                } else {
-                    Err(BpfdError::InvalidInterface)
-                };
-
-                // If program was successfully loaded, allow map access by bpfd group members.
-                if let Ok(uuid) = &res {
-                    let maps_dir = format!("{RTDIR_FS_MAPS}/{uuid}");
-                    set_dir_permissions(&maps_dir, MAPS_MODE).await;
-                }
-
-                // Ignore errors as they'll be propagated to caller in the RPC status
-                let _ = responder.send(res);
-            }
-            Command::LoadTC {
-                location,
-                section_name,
-                id,
-                global_data,
-                iface,
-                priority,
-                direction,
-                proceed_on,
-                username,
-                responder,
-            } => {
-                let res = if let Ok(if_index) = get_ifindex(&iface) {
-                    let prog_data =
-                        ProgramData::new(location, section_name, global_data, username).await?;
-
-                    let prog_result = Ok(Program::Tc(TcProgram {
-                        data: prog_data.clone(),
+                    Command::LoadTC {
+                        location,
+                        section_name,
+                        id,
+                        global_data,
+                        iface,
+                        priority,
                         direction,
-                        info: TcProgramInfo {
-                            if_index,
-                            current_position: None,
-                            metadata: command::Metadata {
-                                priority,
-                                // This could have been overridden by image tags
-                                name: prog_data.section_name,
-                                attached: false,
-                            },
-                            proceed_on,
-                            if_name: iface,
-                        },
-                    }));
+                        proceed_on,
+                        username,
+                        responder,
+                    } => {
+                        let res = if let Ok(if_index) = get_ifindex(&iface) {
+                            let prog_data =
+                                ProgramData::new(location, section_name, global_data, username).await?;
 
-                    match prog_result {
-                        Ok(prog) => bpf_manager.add_program(prog, id).await,
-                        Err(e) => Err(e),
+                            let prog_result = Ok(Program::Tc(TcProgram {
+                                data: prog_data.clone(),
+                                direction,
+                                info: TcProgramInfo {
+                                    if_index,
+                                    current_position: None,
+                                    metadata: command::Metadata {
+                                        priority,
+                                        // This could have been overridden by image tags
+                                        name: prog_data.section_name,
+                                        attached: false,
+                                    },
+                                    proceed_on,
+                                    if_name: iface,
+                                },
+                            }));
+
+                            match prog_result {
+                                Ok(prog) => bpf_manager.add_program(prog, id).await,
+                                Err(e) => Err(e),
+                            }
+                        } else {
+                            Err(BpfdError::InvalidInterface)
+                        };
+
+                        // If program was successfully loaded, allow map access by bpfd group members.
+                        if let Ok(uuid) = &res {
+                            let maps_dir = format!("{RTDIR_FS_MAPS}/{}", uuid.clone());
+                            set_dir_permissions(&maps_dir, MAPS_MODE).await;
+                        }
+
+                        // Ignore errors as they'll be propagated to caller in the RPC status
+                        let _ = responder.send(res);
                     }
-                } else {
-                    Err(BpfdError::InvalidInterface)
-                };
+                    Command::LoadTracepoint {
+                        location,
+                        section_name,
+                        id,
+                        global_data,
+                        tracepoint,
+                        username,
+                        responder,
+                    } => {
+                        let res = {
+                            let prog_data =
+                                ProgramData::new(location, section_name, global_data, username).await?;
 
-                // If program was successfully loaded, allow map access by bpfd group members.
-                if let Ok(uuid) = &res {
-                    let maps_dir = format!("{RTDIR_FS_MAPS}/{}", uuid.clone());
-                    set_dir_permissions(&maps_dir, MAPS_MODE).await;
-                }
+                            let prog_result = Ok(Program::Tracepoint(TracepointProgram {
+                                data: prog_data,
+                                info: TracepointProgramInfo { tracepoint },
+                            }));
 
-                // Ignore errors as they'll be propagated to caller in the RPC status
-                let _ = responder.send(res);
-            }
-            Command::LoadTracepoint {
-                location,
-                section_name,
-                id,
-                global_data,
-                tracepoint,
-                username,
-                responder,
-            } => {
-                let res = {
-                    let prog_data =
-                        ProgramData::new(location, section_name, global_data, username).await?;
+                            match prog_result {
+                                Ok(prog) => bpf_manager.add_program(prog, id).await,
+                                Err(e) => Err(e),
+                            }
+                        };
 
-                    let prog_result = Ok(Program::Tracepoint(TracepointProgram {
-                        data: prog_data,
-                        info: TracepointProgramInfo { tracepoint },
-                    }));
+                        // If program was successfully loaded, allow map access by bpfd group members.
+                        if let Ok(uuid) = &res {
+                            let maps_dir = format!("{RTDIR_FS_MAPS}/{uuid}");
+                            set_dir_permissions(&maps_dir, MAPS_MODE).await;
+                        }
 
-                    match prog_result {
-                        Ok(prog) => bpf_manager.add_program(prog, id).await,
-                        Err(e) => Err(e),
+                        // Ignore errors as they'll be propagated to caller in the RPC status
+                        let _ = responder.send(res);
                     }
-                };
-
-                // If program was successfully loaded, allow map access by bpfd group members.
-                if let Ok(uuid) = &res {
-                    let maps_dir = format!("{RTDIR_FS_MAPS}/{uuid}");
-                    set_dir_permissions(&maps_dir, MAPS_MODE).await;
+                    Command::Unload {
+                        id,
+                        username,
+                        responder,
+                    } => {
+                        let res = bpf_manager.remove_program(id, username).await;
+                        // Ignore errors as they'll be propagated to caller in the RPC status
+                        let _ = responder.send(res);
+                    }
+                    Command::List { responder } => {
+                        let progs = bpf_manager.list_programs();
+                        // Ignore errors as they'll be propagated to caller in the RPC status
+                        let _ = responder.send(progs);
+                    }
                 }
-
-                // Ignore errors as they'll be propagated to caller in the RPC status
-                let _ = responder.send(res);
-            }
-            Command::Unload {
-                id,
-                username,
-                responder,
-            } => {
-                let res = bpf_manager.remove_program(id, username).await;
-                // Ignore errors as they'll be propagated to caller in the RPC status
-                let _ = responder.send(res);
-            }
-            Command::List { responder } => {
-                let progs = bpf_manager.list_programs();
-                // Ignore errors as they'll be propagated to caller in the RPC status
-                let _ = responder.send(progs);
             }
         }
+    }
+
+    for handle in shutdown_senders {
+        handle.send(()).unwrap_or_default();
     }
 
     for handle in task_handles {
