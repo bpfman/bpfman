11af61cc4da43b7800c542046661fff32c2e0723
Dave Tucker <dave@dtucker.co.uk>
2023-12-11 18:59:57 +0000

    packit: Fix sed command
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- .packit.sh
+++ .packit.sh
@@ -6,4 +6,4 @@ GIT_SHORT_SHA=$(git rev-parse --short HEAD)
 sed -i "s/GITSHA/${GIT_SHA}/g" bpfman.spec
 sed -i "s/GITSHORTSHA/${GIT_SHORT_SHA}/g" bpfman.spec
 
-sed -i -r \"s/Release:(\s*)\S+/Release:\1${PACKIT_RPMSPEC_RELEASE}%{?dist}/\" bpfman.spec
+sed -i -r "s/Release:(\s*)\S+/Release:\1${PACKIT_RPMSPEC_RELEASE}%{?dist}/" bpfman.spec
