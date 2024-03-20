0e3eeddf4f0170912ddc7e4d870907c23a2cadeb
Andrew Stoycos <astoycos@redhat.com>
2024-03-20 12:15:37 -0400

    add public-api checks
    Now that bpfman is officially a library this adds
    a public api check to ensure that changes to our
    public api are tightly controlled and explicitly
    "blessed" by contributors.  It will maintain
    public api text files for the bpfman, bpfman-api,
    and bpfman-csi library crates.
    
    Add a short doc section.
    
    Signed-off-by: Andrew Stoycos <astoycos@redhat.com>

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -4,11 +4,13 @@ mod build_manpage;
 mod copy;
 mod integration_test;
 mod protobuf;
+mod public_api;
 mod run;
 mod workspace;
 
 use std::process::exit;
 
+use cargo_metadata::MetadataCommand;
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -34,10 +36,17 @@ enum Command {
     BuildManPage(build_manpage::Options),
     /// Build the completion scripts for bpfman.
     BuildCompletion(build_completion::Options),
+    /// Generate the public API documentation for bpfman.
+    PublicApi(public_api::Options),
 }
 
 fn main() {
     let opts = Options::parse();
+
+    let metadata = MetadataCommand::new()
+        .no_deps()
+        .exec()
+        .expect("failed to run cargo metadata");
 
     use Command::*;
     let ret = match opts.command {
@@ -48,6 +57,7 @@ fn main() {
         IntegrationTest(opts) => integration_test::test(opts),
         BuildManPage(opts) => build_manpage::build_manpage(opts),
         BuildCompletion(opts) => build_completion::build_completion(opts),
+        PublicApi(opts) => public_api::public_api(opts, metadata),
     };
 
     if let Err(e) = ret {
