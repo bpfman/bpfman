086e315e238760ee8d861e9b4ab319a1508d62c5
Andrew McDermott <amcdermo@redhat.com>
2025-02-19 07:16:58 +0000

    Migrate from SLED to SQLite
    This is a copy of the PoC I started in
    https://github.com/frobware/bpfman-hacks/tree/main/s2s.
    
    Lots to be done here. Staging this to start the ball rolling.
    
    Signed-off-by: Andrew McDermott <amcdermo@redhat.com>

---
+++ bpfman/src/bin/sqlite-self-check/main.rs
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of bpfman
+
+// XXX Can be removed in the future.
+fn main() -> anyhow::Result<()> {
+    // Temporary check to confirm whether SQLite is linked statically
+    // or dynamically. Verify linkage using: `ldd
+    // ./target/debug/bpfman`
+    //
+    // Since the `bundled` feature is enabled in `Cargo.toml` for
+    // `libsqlite3-sys`, SQLite should be statically linked, removing
+    // the dependency on system-installed SQLite.
+    //
+    // Additionally, this test ensures that the database schema is
+    // fully embedded within the binary. You can move the
+    // `migrations/` directory aside and still successfully create a
+    // database, confirming that the schema does not rely on external
+    // migration files.
+    let _ = bpfman::establish_sqlite_connection(":memory:")?;
+    Ok(())
+}
